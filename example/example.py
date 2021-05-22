11f66cb4732a4bd5293f2e2dabb46df2504d7395
Chris Dellomes <cjdellomes@gmail.com>
2021-05-21 20:38:01 -0400

    Adjust test service to respond to URL link
    Respond with a thread reply to reduce cluttering

--- example/example.py
+++ example/example.py
@@ -1,24 +1,26 @@
 from slackeventsapi import SlackEventAdapter
 from slack import WebClient
 import os
+import slack_config
 
 # Our app's Slack Event Adapter for receiving actions via the Events API
-slack_signing_secret = os.environ["SLACK_SIGNING_SECRET"]
+slack_signing_secret = slack_config.SIGNING_SECRET
 slack_events_adapter = SlackEventAdapter(slack_signing_secret, "/slack/events")
 
 # Create a SlackClient for your bot to use for Web API requests
-slack_bot_token = os.environ["SLACK_BOT_TOKEN"]
+slack_bot_token = slack_config.BOT_TOKEN
 slack_client = WebClient(slack_bot_token)
 
-# Example responder to greetings
+
 @slack_events_adapter.on("message")
 def handle_message(event_data):
     message = event_data["event"]
-    # If the incoming message contains "hi", then respond with a "Hello" message
-    if message.get("subtype") is None and "hi" in message.get('text'):
+    if message.get("subtype") is None and "test.url.com" in message.get('text'):
         channel = message["channel"]
-        message = "Hello <@%s>! :tada:" % message["user"]
-        slack_client.chat_postMessage(channel=channel, text=message)
+        thread_ts = message.get("ts")
+        #message = "Hello <@%s>! :tada:" % message["user"]
+        message = "If you had a question regarding a ticket, please leave a comment in the ticket itself. It's the best way for us to track the issue and respond in a timely manner."
+        slack_client.chat_postMessage(channel=channel, thread_ts=thread_ts, text=message)
 
 
 # Example reaction emoji echo
@@ -37,4 +39,4 @@ def error_handler(err):
 
 # Once we have our event listeners configured, we can start the
 # Flask server with the default `/events` endpoint on port 3000
-slack_events_adapter.start(port=3000)
+slack_events_adapter.start(port=8080)
